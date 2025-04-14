e60f7d65ab4af3c3b06cd74e1ef2858172099e60
natanael carvalho dos santos <natanaelc7@gmail.com>
2025-04-14 11:20:11 -0300

    correcao do arquivo

--- src/components/common/HeroSlide.jsx
+++ src/components/common/HeroSlide.jsx
@@ -9,7 +9,7 @@ import {
   CarouselPrevious,
   CarouselNext,
   CarouselDots,
-} from "../ui/Carousel"
+} from "../../components/ui/Carousel"
 import { useSiteContext } from "../../context/site-context"
 
 export const HeroSlider = () => {
