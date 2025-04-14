56ac004280145791fa37592f3cf829a2382611e2
natanael carvalho dos santos <natanaelc7@gmail.com>
2025-04-14 12:51:46 -0300

    corrigindo caminho

--- src/components/common/HeroSlide.jsx
+++ src/components/common/HeroSlide.jsx
@@ -9,7 +9,7 @@ import {
   CarouselPrevious,
   CarouselNext,
   CarouselDots,
-} from "../../components/ui/carousel.jsx"
+} from "../ui/Carousel.jsx"
 import { useSiteContext } from "../../context/site-context"
 
 export const HeroSlide = () => {
